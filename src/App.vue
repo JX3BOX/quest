01b5a15af23525e0326c619d4694e17ef24df6e2
iRuxu <rx6@qq.com>
2021-09-25 00:24:49 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,15 @@
             :adminEnable="false"
             :feedbackEnable="true"
         >
-            <img slot="logo" svg-inline src="./assets/img/quest.svg"/>
-            <BreadcrumbInfo/>
+            <img slot="logo" svg-inline :src="getAppIcon('quest')" />
+            <BreadcrumbInfo />
         </Breadcrumb>
         <LeftSidebar>
-            <Nav/>
+            <Nav />
         </LeftSidebar>
         <Main :withoutRight="true">
             <!-- 路由页面内容 -->
-            <router-view/>
+            <router-view />
             <!-- 右侧咨询 -->
             <!-- <RightSidebar> -->
             <!-- </RightSidebar> -->
@@ -27,30 +27,36 @@
 </template>
 
 <script>
-    import Nav from "./components/Nav.vue";
-    import BreadcrumbInfo from "./components/BreadcrumbInfo.vue";
-
-    export default {
-        name: "App",
-        components: {
-            Nav,
-            BreadcrumbInfo,
-        },
-        watch: {
-            $route: {
-                immediate: true,
-                handler() {
-                    if (typeof this.$route.params.class_id === 'undefined') {
-                        this.$store.state.sidebar.class_id = null;
-                    } else {
-                        this.$store.state.sidebar.class_id = this.$route.params.class_id ? this.$route.params.class_id : 0;
-                    }
+import Nav from "./components/Nav.vue";
+import BreadcrumbInfo from "./components/BreadcrumbInfo.vue";
+import { getRewrite, getAppIcon } from "@jx3box/jx3box-common/js/utils";
+export default {
+    name: "App",
+    components: {
+        Nav,
+        BreadcrumbInfo,
+    },
+    watch: {
+        $route: {
+            immediate: true,
+            handler() {
+                if (typeof this.$route.params.class_id === "undefined") {
+                    this.$store.state.sidebar.class_id = null;
+                } else {
+                    this.$store.state.sidebar.class_id = this.$route.params
+                        .class_id
+                        ? this.$route.params.class_id
+                        : 0;
                 }
             },
-        }
-    };
+        },
+    },
+    methods : {
+        getAppIcon
+    }
+};
 </script>
 
 <style lang="less">
-    @import "./assets/css/app.less";
+@import "./assets/css/app.less";
 </style>
